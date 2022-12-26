f909b0e500924c384b72cc0a4b93a5fe0d59244f
viet tu <quyen.luminous@gmail.com>
2022-12-26 17:44:57 +0700

    (!) v0.1 nft admin

--- app/src/NameSpace.tsx
+++ app/src/NameSpace.tsx
@@ -1,5 +1,4 @@
-import {Button, InputGroup} from "react-bootstrap";
-import Form from "react-bootstrap/Form";
+import {Button, InputGroup, Table, Form} from "react-bootstrap";
 import React, {useState} from "react";
 import { ethers } from "ethers";
 import ENS from './abi/ENS.json';
@@ -11,7 +10,9 @@ import BaseRegistrarImplementation from './abi/BaseRegistrarImplementation.json'
 import BIC from './abi/BIC.json';
 import BICRegistrarController from './abi/BICRegistrarController.json';
 import { useAccount } from 'wagmi';
-
+import axios from 'axios';
+import {namehash} from "ethers/lib/utils";
+import {formatFixed} from "@ethersproject/bignumber/src.ts/fixednumber";
 function NameSpace() {
 
     const DAYS = 24 * 60 * 60
@@ -40,7 +41,9 @@ function NameSpace() {
     const [rootOwner, setRootOwner] = useState('')
     const [name, setName] = useState('')
     const [price, setPrice] = useState('0')
-
+    const [isInit, setIsInit] = useState(false)
+    const [eventNamespaceNftData, setEventNamespaceNftData] = useState<any[]>([])
+    const eventNamespaceNftUrl = `https://api-testnet.bscscan.com/api?module=logs&action=getLogs&fromBlock=0&toBlock=lastest&address=0xee87ba582519c11b2541cc0aae7a02e37b647784&topic0=0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62&topic0_2_opr=and&topic2=0x0000000000000000000000000000000000000000000000000000000000000000&apikey=${process.env.REACT_APP_BSC_API_KEY}`
     async function registerName(
         name: string,
         txOptions = { value: BUFFERED_REGISTRATION_COST },
@@ -77,8 +80,9 @@ function NameSpace() {
     }
 
     async function getPriceAndSetName(newString: string) {
-        console.log('newString: ', newString)
-        if(newString.length !== name.length) {
+        if(newString.length === 0) {
+            setPrice('0')
+        } else if(newString.length !== name.length) {
             bicRegistrarController.rentPrice(newString, REGISTRATION_TIME).then(
                 (price: any) =>
                     setPrice(ethers.utils.formatEther(price.base.toString()))
@@ -87,24 +91,73 @@ function NameSpace() {
         setName(newString)
     }
 
-    if(isConnected) {
+    const initInfo = async () => {
         const bicNode = ethers.utils.namehash('bic')
         ens.owner(bicNode).then((addr: string) => setBicOwner(addr))
         ens.owner(ethers.constants.HashZero).then((addr: string) => setRootOwner(addr))
+
+        // to avoid rate limit
+        if(!eventNamespaceNftData.length) {
+            const data = await axios.get(eventNamespaceNftUrl)
+            if(data.data) {
+                const ids = data.data.result.map((e: { data: string; }) => e.data.substring(0,66))
+                const nftData = []
+                for(const id of ids) {
+                    const name = ethers.utils._toEscapedUtf8String(
+                        await nameWrapper.names(id)
+                    )
+                    nftData.push({
+                        id: id,
+                        owner: await nameWrapper.ownerOf(id),
+                        name: name
+                    })
+                }
+
+                setEventNamespaceNftData(nftData)
+            }
+        }
+    }
+
+    if(isConnected) {
+        if(!isInit) {
+            setIsInit(true)
+            initInfo()
+        }
     }
     return (
         <>
             <h1>Namespace Admin:</h1>
             <h2>Root owner <a href={`https://testnet.bscscan.com/address/${rootOwner}`}>{rootOwner}</a></h2>
             <h2>Bic owner <a href={`https://testnet.bscscan.com/address/${bicOwner}`}>{bicOwner}</a></h2>
-            <h2>Your ENS: </h2>
+            <h2>ENS register: </h2>
             <InputGroup className="mb-3">
                 <InputGroup.Text id="basic-addon3">
                     Namespace ({price} BIC):
                 </InputGroup.Text>
                 <Form.Control id="basic-url" aria-describedby="basic-addon3" value={name} onChange={(event) => getPriceAndSetName(event.target.value)}/>
-                <Button onClick={() => registerName(name)}>Create</Button>
+                <Button onClick={() => registerName(name)}>Commit</Button>
+                <Button>Registry</Button>
             </InputGroup>
+            <h2>ENS namespace inventory: </h2>
+            <Table striped bordered hover>
+                <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Owner</th>
+                    <th>Name</th>
+                    <th>Shopping</th>
+                </tr>
+                </thead>
+                <tbody>
+                {eventNamespaceNftData && eventNamespaceNftData.map(e => (<tr>
+                    <td>{ethers.utils.formatUnits(e.id, 0)}</td>
+                    <td>{e.owner}</td>
+                    <td>{e.name}</td>
+                    <td><a href={`https://testnets.opensea.io/assets/bsc-testnet/${nameWrapper.address}/${ethers.utils.formatUnits(e.id, 0)}`} target="_blank" rel="noopener noreferrer">View on Opensea</a></td>
+                </tr>))
+                }
+                </tbody>
+            </Table>
         </>
     )
 }
